c3931b856b4fc3f7cef44eac27d275eb1ec4bdae
Kirubi1526 <149428688+Kirubi1526@users.noreply.github.com>
2024-05-27 16:21:01 +0300

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -20,9 +20,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Enter your session-id",
-BOT_NUMBER: process.env.BOT_NUMBER || "Enter your botnumber (without + mark and spaces)",
-PASSWORD: process.env.PASSWORD || "Enter you like password",
-GITHUB_USERNAME: process.env.GITHUB_USERNAME || "Enter your github username",
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "Enter your GITHUB_AUTH_TOKEN"
+SESSION_ID: process.env.SESSION_ID || "PRABATH-MD~5j8ihRwI#EGDvegY66U1JjZVuUN5zWqDDXc727g6Bg2VuBhR0WAU",
+BOT_NUMBER: process.env.BOT_NUMBER || "254111979755",
+PASSWORD: process.env.PASSWORD || "Juliawangui06",
+GITHUB_USERNAME: process.env.GITHUB_USERNAME || "kirubi1526",
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "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"
 };
