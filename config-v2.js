862e3c226a80992540932e3ba14a60148dc7edb5
hasnainwbking55 <168891161+hasnainwbking55@users.noreply.github.com>
2024-07-06 01:49:01 +0000

    Pending changes exported from your codespace

--- config-v2.js
+++ config-v2.js
@@ -1,5 +1,5 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "923106397166"
 
 
 module.exports = {
