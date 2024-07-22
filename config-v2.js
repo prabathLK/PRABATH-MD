0cf5b5a0dd54bc1cb9575b0367392b7d0217f8f6
RANJITHLASANTHA <ranjilasa73@gmail.com>
2024-07-22 09:01:52 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,5 +1,5 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "94776290455"
 
 
 module.exports = {
