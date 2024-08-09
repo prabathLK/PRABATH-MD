117a90265e7db92b48001288859a1fbaf4ffef16
disindu123 <disindudamsandu@gmail.com>
2024-08-09 22:09:13 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,5 +1,8 @@
-const GITHUB_AUTH_TOKEN = "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"
+const GITHUB_AUTH_TOKEN = "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"
+const SESSION_ID = "PRABATH-MD~l3EQ3bbb#uK3XDkjhs-NJDAL34zkkMfiuXDLrwti_itlTFNglG_4"
 const BOT_NUMBER = "94775704025"
+const OWNER_NAME = "💫Disindu Damsandu💫"
+const OWNER_NUMBER = "94775704025"
 
 
 module.exports = {
